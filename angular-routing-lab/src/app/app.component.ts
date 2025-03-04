44b160610665583aeb513e4beb006bf7fcf63d1e
UBSITCerezo <20234032@s.ubaguio.edu>
2025-03-04 16:45:56 +0800

    commited changes

--- angular-routing-lab/src/app/app.component.ts
+++ angular-routing-lab/src/app/app.component.ts
@@ -8,13 +8,7 @@ import { ProfileComponent } from './about/profile/profile.component';
 
 @Component({
   selector: 'app-root',
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 225d4dc (updated components)
-=======
   imports: [RouterOutlet, HomeComponent, AboutComponent, ContactComponent, ProfileComponent],
->>>>>>> 0218aab6b0d2e6b94ef0c49a0afdc800a92d6618
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
