96d42bc4cd5f264a53ed6eae5ee36f93f658451d
UBSITAlhambra <20124561@s.ubaguio.edu>
2025-03-04 15:34:48 +0800

    Create a new component for a child route, e.g., Profile under About

--- angular-routing-lab/src/app/app.component.ts
+++ angular-routing-lab/src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
+import { AppRoutingModule } from './app-routing/app-routing.module';
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HomeComponent, AboutComponent, ContactComponent, ],
+  imports: [RouterOutlet, HomeComponent, AboutComponent, ContactComponent, AppRoutingModule ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
