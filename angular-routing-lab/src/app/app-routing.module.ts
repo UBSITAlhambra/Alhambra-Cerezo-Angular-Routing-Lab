7a0bbfae5871b30b2d10f571c6d4ca932523330e
UBSITCerezo <20234032@s.ubaguio.edu>
2025-03-04 16:19:50 +0800

    implemented lazy loading modules and route guards

--- angular-routing-lab/src/app/app-routing.module.ts
+++ angular-routing-lab/src/app/app-routing.module.ts
@@ -13,6 +13,7 @@ const routes: Routes = [
  import { AboutComponent } from './about/about.component';
  import { ContactComponent } from './contact/contact.component';
 import { ProfileComponent } from './about/profile/profile.component';
+import { AdminGuard } from './admin/admin.guard';
 
 
  const routes: Routes = [
@@ -22,7 +23,8 @@ import { ProfileComponent } from './about/profile/profile.component';
   { path: 'about', component: AboutComponent, children: [
     { path: 'profile', component: ProfileComponent }
   ]},
-  { path: 'contact', component: ContactComponent }
+  { path: 'contact', component: ContactComponent },
+  { path: 'admin', loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule), canActivate: [AdminGuard] }
 ];
 
 
