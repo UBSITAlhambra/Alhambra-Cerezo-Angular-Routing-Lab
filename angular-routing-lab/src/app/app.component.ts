225d4dc990b3b1b9d6f1b9d3111e310f6e2ed158
UBSITCerezo <20234032@s.ubaguio.edu>
2025-03-04 15:58:40 +0800

    updated components

--- angular-routing-lab/src/app/app.component.ts
+++ angular-routing-lab/src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
+import { ProfileComponent } from './about/profile/profile.component';
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HomeComponent, AboutComponent, ContactComponent, ],
+  imports: [RouterOutlet, HomeComponent, AboutComponent, ContactComponent, ProfileComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
