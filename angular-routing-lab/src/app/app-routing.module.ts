380b18be5022e4482c1d0ce5cb9655e7e4efda86
UBSITAlhambra <20124561@s.ubaguio.edu>
2025-03-06 16:51:40 +0800

    Edited the code <3

--- angular-routing-lab/src/app/app-routing.module.ts
+++ angular-routing-lab/src/app/app-routing.module.ts
@@ -16,7 +16,7 @@ export const routes: Routes = [
   { 
     path: 'admin', 
     loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule), 
-    canActivate: [AdminGuard]  // Protects admin route
+    canActivate: [AdminGuard]  
   }
 ];
 
