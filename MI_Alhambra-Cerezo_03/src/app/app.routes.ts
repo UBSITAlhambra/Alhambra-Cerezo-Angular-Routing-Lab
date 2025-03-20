064661b9c57648c6f8d34b7543bbd57659a1a7a2
UBSITCerezo <20234032@s.ubaguio.edu>
2025-03-20 16:23:01 +0800

    commited auth component with login and register working

--- MI_Alhambra-Cerezo_03/src/app/app.routes.ts
+++ MI_Alhambra-Cerezo_03/src/app/app.routes.ts
@@ -1,3 +1,40 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { GuestComponent } from './components/guest/guest.component';
+import { HomeComponent } from './components/home/home.component';
+import { UsersComponent } from './components/users/users.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { AuthComponent } from './auth/auth.component';
+import { LayoutsComponent } from './layouts/layouts.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'layout',
+    component: LayoutsComponent,  // Parent with Header, Sidebar, Footer
+    children: [
+      { path: 'home', component: HomeComponent },
+      { path: 'users', component: UsersComponent },
+      { path: 'users', component: UsersComponent },
+    ]
+  },
+  {
+    path: 'home',
+    component: HomeComponent,
+  },
+  {
+    path: 'auth',
+    component: AuthComponent,  // Auth layout with login/register
+    children: [
+      { path: 'login', component: LoginComponent },
+      { path: 'register', component: RegisterComponent },
+    ]
+  },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
--- MI_Alhambra-Cerezo_03/src/app/layouts/footer/footer.component.ts
+++ MI_Alhambra-Cerezo_03/src/app/layouts/footer/footer.component.ts
@@ -1,9 +1,8 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
 })
