6407a501eef063a6fdcacaf39a678230a5ffd708
UBSITAlhambra <20124561@s.ubaguio.edu>
2025-03-27 07:00:49 +0800

    Edited Data Binding

--- product-management-app/src/app/components/header/header.component.ts
+++ product-management-app/src/app/components/header/header.component.ts
@@ -1,16 +1,20 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-header', 
-  imports: [RouterModule],
+  imports: [RouterModule, CommonModule],
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent { 
   title = 'Product Management';
+  isDarkTheme = false; 
+  currentPage = 'Home';
 
   toggleTheme() {
-    document.body.classList.toggle('dark-theme');
+    this.isDarkTheme = !this.isDarkTheme;
+    document.body.classList.toggle('dark-theme', this.isDarkTheme);
 }
 }
